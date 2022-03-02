1d23a441c27c7853a603c22845832c1ffc07fbb4
PhanHuuDat <phanhuudat0808@gmail.com>
2022-03-02 21:18:12 +0700

    up database

---
+++ Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Forum.Models
+{
+    public class Account
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Class { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Position { get; set; }
+        public string Avatar { get; set; }
+        
+
+    }
+
+}
---
+++ Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+namespace Forum.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Views { get; set; }
+        public int Likes { get; set; }
+        public int UserId { get; set; }
+        public Account Account { get; set; }
+        public string Image { get; set; }
+    }
+}
